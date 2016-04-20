44056da7e7f03e24331f5ba9964571e544b8adcd
zlodiak <prozaik81-2@yandex.ru>
2016-04-20 18:34:11 +0300

    x y values output ok

--- js/project/views/graphViews.js
+++ js/project/views/graphViews.js
@@ -2,7 +2,16 @@ APP.GraphView = Backbone.View.extend({
 
   initialize: function(options) {
    this.graph_cid = options.graph_cid;
-   this.graphObj = APP.graphCollection.get(this.graph_cid);    
+   this.graphObj = APP.graphCollection.get(this.graph_cid); 
+
+   this.xMin = parseInt(this.graphObj.attributes.xMin, 10);   
+   this.xMax = parseInt(this.graphObj.attributes.xMax, 10);   
+   this.xPeriod = parseInt(this.graphObj.attributes.xPeriod, 10); 
+   this.yMin = parseInt(this.graphObj.attributes.yMin, 10);   
+   this.yMax = parseInt(this.graphObj.attributes.yMax, 10);   
+   this.yPeriod = parseInt(this.graphObj.attributes.yPeriod, 10);    
+
+
   },    
 
   el: $('#contentContainer'), 
@@ -11,8 +20,12 @@ APP.GraphView = Backbone.View.extend({
 
   render: function() {     
     $(this.el).html(this.template(this.graphObj.toJSON()));
+
     this.canvasInit();
     this.renderGuides();
+    this.renderScaleMarks();
+    this.renderScaleValues();
+
     return this;
   }, 
 
@@ -20,23 +33,19 @@ APP.GraphView = Backbone.View.extend({
     this.canvasGraphElem = document.getElementById("canvasGraph");
     this.ctx = this.canvasGraphElem.getContext('2d');
     this.canvasOffset = 40;
+    this.scaleMarkSize = 3;
+    this.yValuesOffset = 4;
+    this.xValuesOffset = 2;
     this.xMax = parseInt(this.graphObj.attributes.xMax, 10);
-    this.yMax = parseInt(this.graphObj.attributes.yMax, 10);
+    this.yMax = parseInt(this.graphObj.attributes.yMax, 10);    
 
     this.canvasGraphElem.width = this.xMax + (this.canvasOffset * 2);
     this.canvasGraphElem.height = this.yMax + (this.canvasOffset * 2); 
-  }, 
-
-  renderGuides: function() {  
-
-
-/*    console.dir(this.graphObj.toJSON());  
-    console.log(xMax);  
-    console.log(yMax);  
-    console.log(canvasGraphElem.width);  
-    console.log(canvasGraphElem.height);  */
 
     this.ctx.translate(this.canvasOffset, this.yMax + this.canvasOffset);
+  }, 
+
+  renderGuides: function() {      
     this.ctx.beginPath();
 
     this.ctx.moveTo(0, 0);
@@ -52,11 +61,36 @@ APP.GraphView = Backbone.View.extend({
   }, 
 
   renderScaleMarks: function() {  
+    this.ctx.beginPath();
 
+    for(var i = this.xMin; i <= this.xMax; i += this.xPeriod) {
+      this.ctx.moveTo(i, -this.scaleMarkSize);
+      this.ctx.lineTo(i, this.scaleMarkSize);
+    };
+
+    for(var i = this.yMin; i <= this.yMax; i += this.yPeriod) {
+      this.ctx.moveTo(-this.scaleMarkSize, this.yMin - i);
+      this.ctx.lineTo(this.scaleMarkSize, this.yMin - i);
+    };    
+
+    this.ctx.lineWidth = 1;
+    this.ctx.strokeStyle = '#000';
+
+    this.ctx.stroke();
   },
 
   renderScaleValues: function() {  
+    for(var i = this.yMin; i <= this.yMax; i += this.yPeriod * 3) {
+      this.ctx.font = "10px Arial";
+      this.ctx.textAlign = "right";
+      this.ctx.fillText(i, -10, (this.yMin - i) + this.yValuesOffset);
+    }; 
 
+    for(var i = this.xMin; i <= this.xMax; i += this.xPeriod * 4) {
+      this.ctx.font = "10px Arial";
+      this.ctx.textAlign = "right";
+      this.ctx.fillText(i, i + this.xValuesOffset, 20);
+    };    
   }    
 
 });
